d5cef73353aa5e54f181a0abe01b0ffd040b7e10
Wei Ning <nwcgnw@gmail.com>
2017-12-05 16:27:33 +1300

    - add queue example (double-ended queue, similar to Python's collections.deque)

--- src/containers/double_ended_queues.lua
+++ src/containers/double_ended_queues.lua
@@ -18,6 +18,35 @@ function append(q, value)
 end
 
 
+function popLeft(q)
+    local first = q.first
+    if first > q.last then
+        error("Queue is empty")
+    end
+    local v = q[first]
+    q[first] = nil
+    q.first = first + 1
+    return v
+end
+
+
+function pop(q)
+    local last = q.last
+    if q.first > last then
+        error("Queue is empty")
+    end
+    local v = q[last]
+    q[last] = nil
+    q.last = last - 1
+    return v
+end
+
+
+function qsize(q)
+    return q.last - q.first + 1
+end
+
+
 function iterateQueue(q, cb)
     for idx = q.first, q.last do
         cb(q[idx])
@@ -30,11 +59,50 @@ function callback(v)
 end
 
 
-function run()
+function testAddNewElements()
     local q = createEmptyQueue()
     pushLeft(q, 121)
     append(q, 312)
+    assert(2 == qsize(q))
     iterateQueue(q, callback)
+end
+
+
+function testRemoveElements()
+    local q  = createEmptyQueue()
+    
+    -- Error: Queue is empty
+    --~ popLeft(q)
+    --~ pop(q)
+    
+    assert(0 == qsize(q))
+    
+    for i = 1, 5 do
+        append(q, i)
+    end
+    
+    assert(5 == qsize(q))
+    assert(1 == popLeft(q))
+    assert(2 == popLeft(q))
+    assert(3 == popLeft(q))
+    assert(2 == qsize(q))
+    
+    for i = 10, 13 do
+        pushLeft(q, i)
+    end
+    
+    assert(6 == qsize(q))
+    assert(5 == pop(q))
+    assert(4 == pop(q))
+    assert(10 == pop(q))
+    assert(3 == qsize(q))
+    
+end
+
+
+function run()
+    testAddNewElements()
+    testRemoveElements()
 end
 
 
