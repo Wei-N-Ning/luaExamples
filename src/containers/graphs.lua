e48fb0d5760e902594799c7ff79d86f425bfa07f
Wei Ning <nwcgnw@gmail.com>
2017-12-06 16:34:10 +1300

    - complete graph creation example; it has lots of space for optimization

--- src/containers/graphs.lua
+++ src/containers/graphs.lua
@@ -19,46 +19,108 @@ function testResourceFile(fileName)
 end
 
 
-function parseNodeLine(line)
-    print("Node", line)
+-- g.root; g.nodes
+function createGraph()
+    local g = {}
+    return g
 end
 
 
-function parseConnectionLine(line)
-    print("Connection", line)
+function parseNodeLine(line, graph)
+    local node = {id=nil, name=nil, adj={}}
+    for t in string.gmatch(line, "[^,]+") do
+        if string.match(t, "^%d+$") then
+            node["id"] = t
+        else
+            -- %s space characters
+            -- The pattern '.-' will expand the least amount
+            node["name"] = string.match(t, "^%s*(.-)%s*$")
+        end
+    end
+    local nodes = graph["nodes"] or {}
+    nodes[#nodes + 1] = node
+    graph["nodes"] = nodes
 end
 
 
-function parseCommentLine(line)
+function parseConnectionLine(line, graph)
+    local nodes = graph["nodes"]
+    if nodes == nil then
+        return
+    end
+    local nodeIds = {}
+    for nodeId in string.gmatch(line, "%d+") do
+        nodeIds[#nodeIds + 1] = nodeId
+    end
+    if #nodeIds <= 1 then
+        return
+    end
+    local from = nodes[tonumber(nodeIds[1]) + 1]
+    for i, nodeId in ipairs(nodeIds) do
+        if i > 1 then
+            local adj = from["adj"]
+            adj[#adj + 1] = nodeId
+        end
+    end
+end
+
+
+function parseCommentLine(line, graph)
+    --~ print("Comment", line)
 end
 
 
 function parseGraphDocument(fileName)
     local filePath = resourceFile(fileName)
     local f = io.open(filePath, 'r')
+    
+    -- section parsers are functions whose interface is:
+    -- func(line, graph)
     local sectionParsers = {
         ["node"]=parseNodeLine, 
         ["conn"]=parseConnectionLine,
     }
     local currentSection = ""
+    local graph = {}
+    
     for line in f:lines() do
         if string.match(line, "^:Node") then
             currentSection = "node"
         elseif string.match(line, "^:Connection") then
             currentSection = "conn"
         elseif string.match(line, "^%#") then
-            parseCommentLine(line)
+            currentSection = "comm"
+            parseCommentLine(line, graph)
         elseif string.match(line, "%w+") == nil then
         else
-            sectionParsers[currentSection](line)
+            sectionParsers[currentSection](line, graph)
+        end
+    end
+    
+    return graph
+end
+
+
+function iterateNodes(g, callback)
+    local nodes = g["nodes"]
+    if nodes ~= nil then
+        for _, n in ipairs(nodes) do
+            callback(n)
         end
     end
 end
 
 
+function printNode(n)
+    print("Node: ", n["id"], n["name"])
+    print("    Connected to: ", table.concat(n["adj"], "; "))
+end
+
+
 function run()
     testResourceFile("documents/simpleGraph.txt")
-    local n = parseGraphDocument("documents/simpleGraph.txt")
+    local g = parseGraphDocument("documents/simpleGraph.txt")
+    iterateNodes(g, printNode)
 end
 
 
