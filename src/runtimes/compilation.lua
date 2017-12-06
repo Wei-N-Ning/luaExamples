9eae598af9c15c86d5891d132e1ed7d3a6c4f4f0
Wei Ning <nwcgnw@gmail.com>
2017-12-07 08:58:36 +1300

    - examples of exception/error handling in Lua, precompiling code (luac -o)

--- src/runtimes/compilation.lua
+++ src/runtimes/compilation.lua
@@ -1,1 +1,49 @@
+-- compile this source file to a binary (opcodes):
+-- luac -o /tmp/prog.lc ./compilation.lua
 
+-- then execute the binary:
+-- lua /tmp/prog.lc
+
+
+function loadSourceFileExpectSuccess()
+    local _ =  [[
+function run()
+    print("called")
+end
+
+run()
+    ]]
+    local filePath = "/tmp/lua_compilation_success.lua"
+    local f = assert(io.open(filePath, "w"))
+    f:write(_)
+    f:close()
+    local func = assert(loadfile(filePath))
+    func()
+end
+
+
+function loadSourceFileExpectFailure()
+    local _ =  [[
+function -- run()
+    print("called")
+end
+
+run()
+    ]]
+        local filePath = "/tmp/lua_compilation_failure.lua"
+    local f = assert(io.open(filePath, "w"))
+    f:write(_)
+    f:close()
+    
+    -- assert error
+    --~ local func = assert(loadfile(filePath))
+end
+
+
+function run()
+    loadSourceFileExpectSuccess()
+    loadSourceFileExpectFailure()
+end
+
+
+run()
--- src/runtimes/errors.lua
+++ src/runtimes/errors.lua
@@ -1,1 +1,26 @@
 
+
+function errorHandlingDemo()
+    
+    -- Lua book:
+    -- Suppose we want to run a piece of Lua code and to catch any 
+    -- error raised while running that code. Our first step is to 
+    -- encapsulate that piece of code in a function;
+    
+    -- annoymous function is executed automatically
+    local ok, msg = pcall(
+        function ()
+            error("asd")
+        end
+    )
+    print(ok)
+    print(msg)
+end
+
+
+function run()
+    errorHandlingDemo()
+end
+
+
+run()
