1cfbd97b73584b499ae173d18b56b9d37bebd256
Wei Ning <nwcgnw@gmail.com>
2017-12-22 11:01:09 +1300

    - more details in closures.lua example

--- src/functions/closures.lua
+++ src/functions/closures.lua
@@ -18,6 +18,12 @@ local function demoClosureWithUpvalues()
     incr()
     incr()
     assert(4 == incr())
+    
+    -- one can access to the upvalue explicitly via debug.getupvalue()
+    -- but this BREAKS ENCAPSULATION !!
+    -- see: https://www.lua.org/pil/23.1.2.html
+    var, value = debug.getupvalue(incr, 1)
+    assert(4 == value)
 end
 
 
