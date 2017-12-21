6efb110a262b6540dc527423a97f786bc77bc1d2
Wei Ning <nwcgnw@gmail.com>
2017-12-22 11:13:02 +1300

    - add example in closures.lua showing how to use closure to mimic objects (instance-state and behaviors)

--- src/functions/closures.lua
+++ src/functions/closures.lua
@@ -24,11 +24,43 @@ local function demoClosureWithUpvalues()
     -- see: https://www.lua.org/pil/23.1.2.html
     var, value = debug.getupvalue(incr, 1)
     assert(4 == value)
+    
+    -- each function constructed by incrFactory() has its own closure
+    -- and upvalue. 
+    -- I can take advantage of this feature and implement some object-
+    -- like functors 
+    incr2 = incrFactory()
+    assert(1 == incr2())
+    incr2()
+    assert(3 == incr2())
+    assert(5 == incr())
+end
+
+
+local function createObject()
+    local state = {value=0}
+    
+    local function setter(v)  -- modify state
+        state.value = v
+    end
+    
+    local function getter()  -- read value from state
+        return state.value
+    end
+    
+    return {setter=setter, getter=getter}
 end
 
 
 local function run()
     demoClosureWithUpvalues()
+    
+    local obj = createObject()
+    assert(0 == obj.getter())
+    obj.setter(101)
+    assert(101 == obj.getter())
+    
+    assert(nil == state)  -- state is fully encapsulated & insulated
 end
 
 
